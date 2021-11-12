04a5247251b5605a405d8165dced8fcd77f63bdf
jordanx8 <60443094+jordanx8@users.noreply.github.com>
2021-11-11 21:49:01 -0600

    adding simple styling

--- src/components/PlayerTable.tsx
+++ src/components/PlayerTable.tsx
@@ -3,7 +3,7 @@ import PlayerRow from "./PlayerRow"
 const PlayerTable = ({data, error, loading}: any) => {
     return (
         <div>
-            <table>
+            <table style={{backgroundColor: "steelblue"}}>
                 <tr>
                     <th>Rank</th>
                     <th>Name</th>
@@ -12,7 +12,7 @@ const PlayerTable = ({data, error, loading}: any) => {
                     <th>Next Game</th>
                 </tr>
                 {(loading ? <h1>Loading...</h1> : (error ? <h1>Error</h1> : data.players.map(({ rank, name, position, school, nextgame }: { rank: any, name: any, position: any, school: any, nextgame: any }) => (
-                    <tr>
+                    <tr style={{ backgroundColor: "lightgray", borderWidth: '10'}}>
                         <PlayerRow rank={rank} name={name} position={position} school={school} nextgame={nextgame} />
                     </tr>
                 ))))}
